ed51341e190d71986e9355fd8136904715bbddc2
magidian <manmandoo123@gmail.com>
2022-02-16 18:28:26 +0800

    routing bug fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import Home from "./Routes/Home";
 import CoinPage from "./Routes/CoinPage";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/CoinPage/:id" element={<CoinPage />} />
-        </Routes>
-      </Router>
-    </div>
+    // <Router>
+      <HashRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/CoinPage/:id" element={<CoinPage />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    // </Router>
   );
 }
 
